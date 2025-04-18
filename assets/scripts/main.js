9b72e15af925a87629502732ab38c1ef505fe6a4
Wedad Osama <welkammash@gmail.com>
2025-04-18 20:21:36 +0200

    Add:Section Hero

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,9 @@
 document.getElementById("toggle-theme").onclick = () => {
   document.body.classList.toggle("dark-theme");
   const img = document.getElementById("toggle-theme");
-  img.src = document.body.classList.contains("dark-theme")
-    ? "assets/icons/dark.webp"
-    : "assets/icons/light.webp";
+  img.src = document.body.classList.contains("dark-theme") ? "assets/icons/dark.webp" : "assets/icons/light.webp";
 };
 
-// Menu toggle on small screens
 document.getElementById("menu-toggle").addEventListener("change", function () {
   const navMenu = document.getElementById("nav-menu");
   if (this.checked) {
